c028eb749b961cf1ca902eb42f77466594a9b13d
taimurwaheed <taimurwaheed34570@gmail.com>
2025-08-08 15:24:21 +0500

    feat: add/test react-query + theme colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import AppRoutes from "./router/routes";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { UserProvider } from "./context/UserContext ";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "./theme/theme";
+
+const queryClient = new QueryClient();
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <ThemeProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <UserProvider>
+          <AppRoutes />
+        </UserProvider>
+      </QueryClientProvider>
+    </ThemeProvider >
+  );
 }
 
 export default App;
