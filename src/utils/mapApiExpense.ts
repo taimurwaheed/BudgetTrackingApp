299da7cc63ab178505b5f417a6a876f67cda59db
taimurwaheed <taimurwaheed34570@gmail.com>
2025-08-25 13:38:18 +0500

    feat: refactor expense types

---
+++ src/utils/mapApiExpense.ts
@@ -0,0 +1,10 @@
+import type { Expense } from "../types/expense.types";
+
+export const mapApiExpense = (expense: any): Expense => ({
+    expenseId: expense.expenseId,
+    expense: expense.expense,
+    totalExpenditure: expense.totalExpenditure,
+    price: expense.price,
+    date: new Date(expense.date),
+    userId: expense.userId,
+});
