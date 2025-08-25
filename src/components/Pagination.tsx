71318f3825413393fb9a3fc3e039ba65f6524087
taimurwaheed <taimurwaheed34570@gmail.com>
2025-08-25 13:30:35 +0500

    feat: add Pagination

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,22 @@
+// src/hooks/usePagination.ts
+import { useState } from "react";
+
+export function usePagination<T>(data: T[], itemsPerPage: number = 5) {
+    const [page, setPage] = useState(1);
+
+    const handlePageChange = (_event: React.ChangeEvent<unknown>, value: number) => {
+        setPage(value);
+    };
+
+    const pageCount = Math.ceil((data?.length || 0) / itemsPerPage);
+
+    const startIndex = (page - 1) * itemsPerPage;
+    const currentData = data?.slice(startIndex, startIndex + itemsPerPage) ?? [];
+
+    return {
+        page,
+        pageCount,
+        currentData,
+        handlePageChange,
+    };
+}
