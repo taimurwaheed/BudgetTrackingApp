b1fefe19f13d5e3f740b513b0baf51d66a7fb0d1
taimurwaheed <taimurwaheed34570@gmail.com>
2025-08-25 13:33:30 +0500

    feat: add modals

---
+++ src/components/AddExpenseModal.styles.ts
@@ -0,0 +1,25 @@
+import { styled } from "@mui/material/styles";
+import { Box, Button } from "@mui/material";
+
+export const ModalBox = styled("form")(({ theme }) => ({
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(4),
+    borderRadius: 2,
+    boxShadow: theme.shadows[5],
+    width: 400,
+    display: "flex",
+    flexDirection: "column",
+    gap: theme.spacing(2),
+}));
+
+export const SubmitButtonBox = styled(Box)({
+    marginTop: 16,
+});
+
+export const AddExpenseBtn = styled(Button)({
+    marginTop: 4,
+});
---
+++ src/components/EditExpenseModal.styles.ts
@@ -0,0 +1,21 @@
+import { styled } from "@mui/material/styles";
+import { Box } from "@mui/material";
+
+export const ModalBox = styled(Box)(({ theme }) => ({
+    padding: theme.spacing(4),
+    backgroundColor: theme.palette.background.paper,
+    margin: "auto",
+    marginTop: theme.spacing(10),
+    width: 400,
+    borderRadius: 2,
+    boxShadow: theme.shadows[5],
+    display: "flex",
+    flexDirection: "column",
+    gap: theme.spacing(2),
+}));
+
+export const FormActions = styled(Box)({
+    marginTop: 16,
+    display: "flex",
+    justifyContent: "space-between",
+});
