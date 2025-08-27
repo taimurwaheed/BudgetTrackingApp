a7a98eff385686d431c99ba0909853ecc49ce310
taimurwaheed <taimurwaheed34570@gmail.com>
2025-08-27 17:19:24 +0500

    refact: deployment

--- src/types/form-input.types.ts
+++ src/types/form-input.types.ts
@@ -1,10 +1,11 @@
 import * as yup from "yup";
 import { ExpenseSchema, Loginschema, ResetPasswordSchema, Signupschema } from "../utils/schemas";
 
-export type LoginFormInputs = yup.InferType<typeof Loginschema>;
+// Ensure all fields are treated as required in TS to align with Yup schemas
+export type LoginFormInputs = Required<yup.InferType<typeof Loginschema>>;
 
-export type SignUpFormInputs = yup.InferType<typeof Signupschema>;
+export type SignUpFormInputs = Required<yup.InferType<typeof Signupschema>>;
 
-export type ExpenseFormInputs = yup.InferType<typeof ExpenseSchema>;
+export type ExpenseFormInputs = Required<yup.InferType<typeof ExpenseSchema>>;
 
-export type ResetPasswordFormData = yup.InferType<typeof ResetPasswordSchema>;
+export type ResetPasswordFormData = Required<yup.InferType<typeof ResetPasswordSchema>>;
